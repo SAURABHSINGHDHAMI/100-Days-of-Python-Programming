9a60eda009c24706a4f15218cdaa323498b5572b
SAURABH SINGH DHAMI <95751390+SAURABHSINGHDHAMI@users.noreply.github.com>
2024-01-15 12:21:12 +0530

    Update app.py

--- IRCTC Clone/app.py
+++ IRCTC Clone/app.py
@@ -1,9 +1,8 @@
 import requests
 
 class IRCTC:
-    
     def __init__(self):
-        
+        # User prompt for action selection
         user_input = input("""How would you like to proceed?
         1. Enter 1 to check live train station
         2. Enter 2 to check PNR
@@ -17,17 +16,22 @@ class IRCTC:
             self.train_schedule()     
     
     def train_schedule(self):
+        # User input for train number
         train_no = input("Enter the train no: ")
         self.fetch_data(train_no)
         
     def fetch_data(self, train_no):
+        # API request to fetch train schedule data
         data = requests.get("https://indianrailapi.com/api/v2/TrainSchedule/apikey/30c382602bfa67c8a7c580e6cfe2becb/TrainNumber/{}".format(train_no))
         
         data = data.json()
         
+        # Displaying the train route details
         print(data['Route'])
         
         for i in data['Route']:
+            # Printing station details
             print(i['StationName'], "|", i["ArrivalTime"], "|", i["DepartureTime"], "|", i["Distance"], "kms")
         
+# Creating an instance of the IRCTC class to run the code
 obj = IRCTC()
